03c3298cd0478a4248cddc361fd87f4ff4c59ff1
JoseLuis <jvazquez3@nigul.cide.es>
2023-12-13 01:51:29 +0100

    INVADERS

---
+++ SpaceInvaders/elements.py
@@ -0,0 +1,42 @@
+import pygame
+
+
+class Aircraft:
+    def __init__(self) -> None:
+        self.posleft = 200
+        self.postop = 200
+        aircraft_img = [pygame.image.load("SpaceInvaders/avion1.png"),pygame.image.load("SpaceInvaders/avion2.png"),pygame.image.load("SpaceInvaders/avion-left.png"),pygame.image.load("SpaceInvaders/avion-right.png"),pygame.image.load("SpaceInvaders/avion-bullet.png")]
+        self.count = 0
+        self.aircraft = [pygame.transform.scale(aircraft_img[0],(90,90)),pygame.transform.scale(aircraft_img[1],(90,90)),pygame.transform.scale(aircraft_img[2],(90,90)),pygame.transform.scale(aircraft_img[3],(90,90)),pygame.transform.scale(aircraft_img[4],(30,30))]
+
+    def paint(self):
+        self.count = (self.count+1)%40
+        screen = pygame.display.get_surface()
+        selected = self.count // 20
+        screen.blit(self.aircraft[selected],(self.posleft, self.postop))
+    def paintleft(self):
+        screen = pygame.display.get_surface()
+        screen.blit(self.aircraft[2],(self.posleft, self.postop))
+    def paintright(self):
+        screen = pygame.display.get_surface()
+        screen.blit(self.aircraft[3],(self.posleft, self.postop))
+        
+    def moveleft(self):
+        if(self.posleft > 0):
+            self.posleft -= 3
+        
+    def moveright(self):
+        if(self.posleft < 480 - 90):
+            self.posleft += 3
+            
+    def movetop(self):
+        if(self.postop > 0):
+            self.postop -= 3
+    def movedown(self):
+        if(self.postop <800 - 90):
+            self.postop += 3
+    def shoot(self):
+        
+class Background:
+    def __init__(self) -> None:
+         bg = ("SpaceInvaders/background.png")
---
+++ SpaceInvaders/invaders.py
@@ -0,0 +1,47 @@
+import pygame
+from elements import Aircraft
+
+pygame.init()
+
+screen = pygame.display.set_mode((480,800))
+
+aircraft = Aircraft()
+#enemies = Enemies()
+exit = False
+clock = pygame.time.Clock()
+FPS = 60
+while not exit:
+    clock.tick(60)
+    #gestionar eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = True
+    #gestionar cambiso 
+    background_image = pygame.image.load("SpaceInvaders/background.png")
+    screen.blit(background_image, [0, 0])
+    
+    keyboard = pygame.key.get_pressed()
+    if not keyboard[pygame.K_LEFT] and not keyboard[pygame.K_RIGHT]:
+       aircraft.paint()
+
+    if keyboard[pygame.K_LEFT]:
+        aircraft.moveleft()
+        aircraft.paintleft()
+
+    if keyboard[pygame.K_UP]:
+        aircraft.movetop()
+        
+    if keyboard[pygame.K_RIGHT]:
+        aircraft.moveright()
+        aircraft.paintright()
+    
+    if keyboard[pygame.K_DOWN]:
+        aircraft.movedown()
+    if keyboard[pygame.K_BACKSPACE]:
+        aircraft.shoot()
+    
+    
+
+    #redibujar juego
+    pygame.display.flip()
+pygame.quit
